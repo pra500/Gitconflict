e4717736fc7d6aa8d534f731e353b3b67c6ce45f
pragti gupta <pragtig6@gmail.com>
2024-12-18 17:01:25 +0530

    a

---
+++ src/test/java/pac1/AutoSuggestionTest.java
@@ -0,0 +1,52 @@
+package pac1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+public class AutoSuggestionTest {
+	
+		WebDriver driver;
+		
+		
+	@Test
+	public void pickdateTest()
+	{	
+				
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);		
+		driver.get("https://practicetestautomation.com/practice-test-login/");		
+		driver.findElement(By.xpath("//a[normalize-space()='Practice']")).click();
+		
+		
+		
+		driver.close();
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+		
+	 
+	  
+	  
+  
+
+}
